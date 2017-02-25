d69c89b61c1f3f666e17aeac00068e32640b8372
Nicholas Swift <nicholas.w.swift@gmail.com>
2017-02-24 19:28:04 -0800

    Initial Commit

---
+++ ListenLive/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ListenLive
+//
+//  Created by Nicholas Swift on 2/24/17.
+//  Copyright © 2017 Nicholas Swift. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
