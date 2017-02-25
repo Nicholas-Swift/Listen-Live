a053fa6a8bff883873f8d5eb2491bfce200a2485
Nicholas Swift <nicholas.w.swift@gmail.com>
2017-02-24 22:11:15 -0800

    Fixed stack views

---
+++ ListenLive/Models/Track.swift
@@ -0,0 +1,25 @@
+//
+//  Track.swift
+//  ListenLive
+//
+//  Created by Nicholas Swift on 2/24/17.
+//  Copyright © 2017 Nicholas Swift. All rights reserved.
+//
+
+import UIKit
+
+class Track {
+    
+    // MARK: - Instance Vars
+    var songId: String!
+    var title: String!
+    var thumbnailURL: String!
+    
+    // MARK: - Init
+    init?(data: [String: AnyObject]) {
+        
+        // Parse json shit here
+        
+    }
+    
+}
---
+++ ListenLive/ViewModels/FeedViewModel.swift
@@ -0,0 +1,29 @@
+//
+//  FeedViewModel.swift
+//  ListenLive
+//
+//  Created by Nicholas Swift on 2/24/17.
+//  Copyright © 2017 Nicholas Swift. All rights reserved.
+//
+
+import Foundation
+
+class FeedViewModel {}
+
+// MARK: - Table View Delegate
+extension FeedViewModel {
+    
+}
+
+// MARK: - Table View Delegate
+extension FeedViewModel {
+    
+    func numberOfSections() -> Int {
+        return 10
+    }
+    
+    func numberOfRows() -> Int {
+        return 1
+    }
+    
+}
