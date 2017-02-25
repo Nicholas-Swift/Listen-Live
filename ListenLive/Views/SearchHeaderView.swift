82cfa6abaeb382b9a9211519e6fb44c0954a9e50
Nicholas Swift <nicholas.w.swift@gmail.com>
2017-02-25 00:48:45 -0800

    Search view controller coming together well

---
+++ ListenLive/Views/SearchHeaderView.swift
@@ -0,0 +1,21 @@
+//
+//  SearchHeaderView.swift
+//  ListenLive
+//
+//  Created by Nicholas Swift on 2/25/17.
+//  Copyright © 2017 Nicholas Swift. All rights reserved.
+//
+
+import UIKit
+
+class SearchHeaderView: UIView {
+    
+    // MARK: - Subviews
+    @IBOutlet weak var titleHeaderLabel: UILabel!
+    
+    // MARK: - View Lifecycle
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+}
