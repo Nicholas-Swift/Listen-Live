4ea864cf72ea80ce5a272b905e5c067c5ae42450
Nicholas Swift <nicholas.w.swift@gmail.com>
2017-02-24 22:15:02 -0800

    stuff

---
+++ ListenLive/Views/FeedNavigationBar.swift
@@ -0,0 +1,24 @@
+//
+//  FeedNavigationBar.swift
+//  ListenLive
+//
+//  Created by Nicholas Swift on 2/24/17.
+//  Copyright © 2017 Nicholas Swift. All rights reserved.
+//
+
+import UIKit
+
+class FeedNavigationBar: UIView {
+    
+    // MARK: - Subviews
+    
+    // MARK: - View Lifecycle
+    init() {
+        <#statements#>
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+    }
+    
+}
